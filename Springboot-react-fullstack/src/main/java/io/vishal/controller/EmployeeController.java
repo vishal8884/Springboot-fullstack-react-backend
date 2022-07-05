abbd38400260e4fda437616a39079223415bc3ed
vishal <vishalambekar459@gmail.com>
2022-07-05 20:20:19 +0530

    find by id added

--- Springboot-react-fullstack/src/main/java/io/vishal/controller/EmployeeController.java
+++ Springboot-react-fullstack/src/main/java/io/vishal/controller/EmployeeController.java
@@ -3,13 +3,16 @@ package io.vishal.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.vishal.exception.ResourceNotFoundException;
 import io.vishal.model.Employee;
 import io.vishal.repository.EmployeeRepository;
 
@@ -29,6 +32,15 @@ public class EmployeeController {
 	@PostMapping("/employees")
 	public Employee createEmployee(@RequestBody Employee employee) {
 		return employeeRepository.save(employee);
+	}
+	
+	//get employee by id rest api
+	@GetMapping("/employees/{id}")
+	public ResponseEntity<Employee> getEmployeeById(@PathVariable Long id) {
+		Employee employee = employeeRepository.findById(id).orElseThrow( () 
+				-> new ResourceNotFoundException("Employee does exist with id :"+id));
+		
+		return ResponseEntity.ok(employee);
 	}
 	
 	
