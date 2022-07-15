d369465a21928ca80c72e09f383188e5de2c4877
vishal <vishalambekar459@gmail.com>
2022-07-16 00:48:52 +0530

    Delete ampping added

--- Springboot-react-fullstack/src/main/java/io/vishal/controller/EmployeeController.java
+++ Springboot-react-fullstack/src/main/java/io/vishal/controller/EmployeeController.java
@@ -1,10 +1,13 @@
 package io.vishal.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -58,6 +61,20 @@ public class EmployeeController {
 		Employee updatedEmployee = employeeRepository.save(employee);
 		
 		return ResponseEntity.ok(updatedEmployee);
+	}
+	
+	
+	// delete employee rest api
+	@DeleteMapping("/employees/{id}")
+	public ResponseEntity<Map<String, Boolean>> deleteEmployee(@PathVariable Long id){
+		Employee employee = employeeRepository.findById(id).orElseThrow( () 
+				-> new ResourceNotFoundException("Employee does exist with id :"+id));
+		
+		employeeRepository.delete(employee);
+		Map<String, Boolean> response = new HashMap<>();
+		response.put("deleted", Boolean.TRUE);
+		
+		return ResponseEntity.ok(response);
 	}
 	
 }
